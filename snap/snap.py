3c9d66d3f9659e800f2bc1a56a58b3eecb9252d8
Justin T. Sampson <justin@krasama.com>
2024-01-20 19:36:42 +0000

    More WIP on snapshot analysis

--- snap/snap.py
+++ snap/snap.py
@@ -1,48 +1,44 @@
 import numpy
-import pyphi
+#import pyphi
 from fractions import Fraction as F
 
-# delta: tuple[tuple[str], tuple[str]] (1st is always one of ABX; 2nd is either one or both of remaining)
-# env = tuple({"A", "B", "X", "I"} - set(delta[0]) - set(delta[1]))
-# state = tuple[tuple[str, bool], ...]
-# sigma: tuple[tuple[tuple[state], tuple[state]], tuple[tuple[state], tuple[state]]]
-# epsilon: tuple[state] (where states include "I" plus possibly one other)
-
-ABX = tuple("A", "B", "X")
-
 def gen_delta():
-    for i in range(3):
-        first = ABX[i]
-        for j in range(i + 1, 3):
-            second = ABX[j]
-            yield ((first,), (second,))
-        rest = list(ABX)
-        del rest[first]
-        yield ((first,), tuple(rest))
+    yield ("A", "B")
+    yield ("A", "X")
+    yield ("A", "BX")
+    yield ("B", "A")
+    yield ("B", "X")
+    yield ("B", "AX")
+    yield ("X", "A")
+    yield ("X", "B")
+    yield ("X", "AB")
+    yield ("AB", "X")
+    yield ("AX", "B")
+    yield ("BX", "A")
 
 def gen_epsilon(delta, prog):
-    others = set(ABX) - set(delta[0]) - set(delta[1])
+    others = set("ABX") - set(delta[0]) - set(delta[1])
     assert len(others) == 0 or len(others) == 1
     other = list(others)[0] if others else None
     for I in prog.I_options:
         if not other:
-            yield ((("I", I),),)
+            yield (f"I{I}",)
         else:
-            yield ((("I", I), (other, 0)))
-            yield ((("I", I), (other, 1)))
-            yield ((("I", I), (other, 0)), (("I", I), (other, 1)))
+            yield (f"{other}0I{I}",)
+            yield (f"{other}1I{I}",)
+            yield (f"{other}0I{I}", f"{other}1I{I}")
 
 def gen_sigma(delta):
     assert len(delta) == 2
     for sigma_sub_0 in gen_sigma_sub(delta[0]):
         for sigma_sub_1 in gen_sigma_sub(delta[1]):
-            return (sigma_sub_0, sigma_sub_1)
+            yield (sigma_sub_0, sigma_sub_1)
 
 def gen_sigma_sub(delta_sub):
     if len(delta_sub) == 1:
         v = delta_sub[0]
-        yield ((((v, 0),),), (((v, 1),),))
-        yield ((((v, 1),),), (((v, 0),),))
+        yield ((f"{v}0",), (f"{v}1",))
+        yield ((f"{v}1",), (f"{v}0",))
     else:
         assert len(delta_sub) == 2
         v0 = delta_sub[0]
@@ -55,26 +51,26 @@ def gen_sigma_sub(delta_sub):
                         on = []
 
                         if a == 0:
-                            off.append(((v0, 0), (v1, 0)))
+                            off.append(f"{v0}0{v1}0")
                         elif a == 1:
-                            on.append(((v0, 0), (v1, 0)))
+                            on.append(f"{v0}0{v1}0")
 
                         if b == 0:
-                            off.append(((v0, 1), (v1, 0)))
+                            off.append(f"{v0}1{v1}0")
                         elif b == 1:
-                            on.append(((v0, 1), (v1, 0)))
+                            on.append(f"{v0}1{v1}0")
 
                         if c == 0:
-                            off.append(((v0, 0), (v1, 1)))
+                            off.append(f"{v0}0{v1}1")
                         elif c == 1:
-                            on.append(((v0, 0), (v1, 1)))
+                            on.append(f"{v0}0{v1}1")
 
                         if d == 0:
-                            off.append(((v0, 1), (v1, 1)))
+                            off.append(f"{v0}1{v1}1")
                         elif d == 1:
-                            on.append(((v0, 1), (v1, 1)))
+                            on.append(f"{v0}1{v1}1")
 
-                        if len(off) > 0 and len(in) > 0:
+                        if len(off) > 0 and len(on) > 0:
                             yield (tuple(off), tuple(on))
 
 def calc_tpm(prog, delta, epsilon, sigma):
@@ -83,37 +79,54 @@ def calc_tpm(prog, delta, epsilon, sigma):
         count = 0
         p0 = 0
         p1 = 0
-        for ...:  # TODO: micro states of element 0
-            for ...:  # TODO: micro states of element 1
-                for ...:  # TODO: micro states of environment
+        for elem_0_micro in sigma[0][macro_start[0]]:
+            for elem_1_micro in sigma[1][macro_start[1]]:
+                for env_micro in epsilon:
                     count += 1
-                    micro_start = ...  # TODO: combine from for loops
+                    micro_start = elem_0_micro + elem_1_micro + env_micro
                     micro_end = do_prog(prog, micro_start)
-                    macro_end = ...  # TODO: infer from micro_end
-                    if macro_end[0]:
-                        p0 += 1
-                    if macro_end[1]:
-                        p1 += 1
+                    macro_end = (
+                        macro_bit(delta, sigma, micro_end, 0),
+                        macro_bit(delta, sigma, micro_end, 1),
+                    )
+                    p0 += macro_end[0]
+                    p1 += macro_end[1]
         tpm.append((F(p0, count), F(p1, count)))
     return tuple(tpm)
 
+def macro_bit(delta, sigma, micro_state, i):
+    restricted_state = ""
+    for v in delta[i]:
+        restricted_state += f"{v}{lookup(micro_state, v)}"
+    if restricted_state in sigma[i][0]:
+        return F(0)
+    elif restricted_state in sigma[i][1]:
+        return F(1)
+    else:
+        return F("1/2")
+
 def calc_phi(tpm, state):
-    TODO
+    return 0  # TODO: call PyPhi
 
 def print_row(prog, tpm, phis):
-    TODO
+    # TODO: turn into LaTeX
+    print(prog)
+    print("; ".join((", ".join((str(f) for f in r)) for r in tpm)))
+    # print(phis)
 
 def lookup(state, reg):
-    return [v for r, v in state if r == reg][0]
+    assert reg in state
+    reg_index = state.index(reg)
+    return int(state[reg_index + 1])
 
 def do_prog(prog, state):
     A, B, X, I = prog(
+        lookup(state, "A"),
+        lookup(state, "B"),
         lookup(state, "X"),
-        lookup(state, "Y"),
-        lookup(state, "Z"),
         lookup(state, "I"),
     )
-    return (("A", A), ("B", B), ("X", X), ("I", I))
+    return f"A{A}B{B}X{X}I{I}"
 
 def prog1(A, B, X, I):
     if I == 3:
@@ -145,10 +158,15 @@ def prog2(A, B, X, I):
 
 prog2.I_options = (4, 5)
 
-for prog in (prog1, prog2):
-    for delta in gen_delta():
-        for epsilon in gen_epsilon(delta, prog):
-            for sigma in gen_sigma(delta):
-                tpm = calc_tpm(prog, delta, epsilon, sigma)
-                phis = [calc_phi(tpm, state) for state in  ((0, 0), (1, 0), (0, 1), (1, 1))]
-                print_row(prog, tpm, phis)
+def main(limit = 1, progs = (prog1, prog2)):
+    count = 0
+    for prog in progs:
+        for delta in gen_delta():
+            for epsilon in gen_epsilon(delta, prog):
+                for sigma in gen_sigma(delta):
+                    tpm = calc_tpm(prog, delta, epsilon, sigma)
+                    phis = [calc_phi(tpm, state) for state in  ((0, 0), (1, 0), (0, 1), (1, 1))]
+                    print_row(prog, tpm, phis)
+                    count += 1
+                    if count >= limit:
+                        return
